2d9f1051e46ec0d35bbbd2fcfaf9540327793d10
name <speseek@gmail.com>
2017-11-18 17:54:30 +0900

    test002

--- src/withmoon/Moon.java
+++ src/withmoon/Moon.java
@@ -2,6 +2,7 @@ package withmoon;
 
 public class Moon {
 	public static void main(String[] args) {
-		System.out.println("흐아아아아피자피자");
+		System.out.println("�쓲�븘�븘�븘�븘�뵾�옄�뵾�옄");
+		String shit = "shit";
 	}
 }
